e56dc6a289aa2184797ad655cf2cb0f87edb65fd
ayushmankumar7 <ayushmankumar7@gmail.com>
2022-03-16 15:57:06 +0530

    first example added

---
+++ examples/hello_world/hello_world.ino
@@ -0,0 +1,12 @@
+#include <first.h>
+
+First pin(13);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  pin.blink(1);
+}
